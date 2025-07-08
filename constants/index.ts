22b1146c1577882d06e7e25211d6eb778ae3dac7
Mankgarube Frederick <mfmafolo@outlook.com>
2025-07-09 00:20:08 +0200

    added constants

---
+++ constants/index.ts
@@ -0,0 +1,15 @@
+export const API_URLS = {
+  getProperties: '/api/properties',
+  getPropertyDetails: '/api/properties/details',
+};
+
+export const CONFIG = {
+  defaultTimeout: 5000,
+  maxRetries: 3,
+};
+
+export const UI_TEXT = {
+  bookNow: 'Book Now',
+  details: 'Details',
+  welcomeMessage: 'Welcome to the Listing App',
+};
